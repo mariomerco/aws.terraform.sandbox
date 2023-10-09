b85bebed8e37a6e885108e50a05b3cbea6bb987c
mariomerco <mario.mercado.c@gmail.com>
2023-10-09 17:22:24 -0500

    backend in a separate file

---
+++ e1/backend.tf
@@ -0,0 +1,8 @@
+# terraform {
+#   backend "s3" {
+#     bucket         = "terraform-backend-terraformbackends3bucket-uxqy4davkhfy"
+#     key            = "testing/e1"
+#     region         = "us-east-1"
+#     dynamodb_table = "terraform-backend-TerraformBackendDynamoDBTable-1KJU6R5SDECD6"
+#   }
+# }

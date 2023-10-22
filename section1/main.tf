59333306110af3717bb74b335899bce4eb5c0b31
mariomerco <mario.mercado.c@gmail.com>
2023-10-21 19:41:30 -0500

    sandboxing

--- section1/main.tf
+++ section1/main.tf
@@ -1,3 +1,12 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.this.id
+  instance_type = "t3.micro"
+
+  user_data              = filebase64("scripts/user_data.sh")
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
+}
+
+
 data "aws_ami" "this" {
   most_recent = true
   owners      = ["amazon"]
@@ -11,7 +20,31 @@ data "aws_ami" "this" {
   }
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.this.id
-  instance_type = "t3.micro"
+data "aws_vpc" "this" {
+  cidr_block = "172.31.0.0/16"
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = data.aws_vpc.this.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
 }

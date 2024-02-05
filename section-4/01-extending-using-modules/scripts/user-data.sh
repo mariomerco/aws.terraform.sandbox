4e5b94597505b882513fbcd44c3b0e8932e23157
mariomerco <mario.mercado.c@gmail.com>
2024-02-05 04:59:51 -0500

    userdata

--- section-4/01-extending-using-modules/scripts/user-data.sh
+++ section-4/01-extending-using-modules/scripts/user-data.sh
@@ -1,10 +1,4 @@
 #! /bin/bash
-# sudo amazon-linux-extras install -y nginx1
-# sudo service nginx start
-# sudo rm /usr/share/nginx/html/index.html
-# echo '<html><head><title>Testing Terraform</title></head><body><h1>You definitely know Terraform!</h1></body></html>' | sudo tee /usr/share/nginx/html/index.html
-
-#! /bin/bash
 
 sudo yum update -y
 sudo yum install -y httpd.x86_64

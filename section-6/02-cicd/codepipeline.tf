e7a72fd445e5ca9480e156032c25df6644d95081
mariomerco <mario.mercado.c@gmail.com>
2024-02-20 21:48:00 -0500

    demo done

--- section-6/02-cicd/codepipeline.tf
+++ section-6/02-cicd/codepipeline.tf
@@ -1,121 +1,187 @@
-# resource "aws_codepipeline" "codepipeline" {
-#   name     = "tf-test-pipeline"
-#   role_arn = aws_iam_role.codepipeline_role.arn
+resource "aws_codepipeline" "codepipeline" {
+  name     = "tf-test-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
 
-#   artifact_store {
-#     location = aws_s3_bucket.codepipeline_bucket.bucket
-#     type     = "S3"
-#   }
+  artifact_store {
+    location = aws_s3_bucket.codepipeline_bucket_artifact_store.bucket
+    type     = "S3"
+  }
 
-#   stage {
-#     name = "Source"
+  stage {
+    name = "Source"
 
-#     action {
-#       name             = "Source"
-#       category         = "Source"
-#       owner            = "AWS"
-#       provider         = "CodeCommit"
-#       version          = "1"
-#       output_artifacts = ["source_output"]
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeCommit"
+      version          = "1"
+      output_artifacts = ["source_artifact"]
 
-#       configuration = {
-#         PollForSourceChanges = false
-#         FullRepositoryId     = aws_codecommit_repository.source_code.repository_name
-#         BranchName           = "main"
-#       }
-#     }
-#   }
+      configuration = {
+        PollForSourceChanges = false
+        RepositoryName       = aws_codecommit_repository.source_code.repository_name
+        BranchName           = "master"
+      }
+    }
+  }
 
-#   stage {
-#     name = "Deploy"
+  stage {
+    name = "Deploy"
 
-#     action {
-#       name            = "Deploy"
-#       category        = "Deploy"
-#       owner           = "AWS"
-#       provider        = "CloudFormation"
-#       input_artifacts = ["build_output"]
-#       version         = "1"
+    action {
+      name            = "Deploy"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source_artifact"]
+      version         = "1"
+      configuration = {
+        ProjectName = aws_codebuild_project.build_project.name
+      }
+    }
+  }
+}
 
-#       configuration = {
-#         ActionMode     = "REPLACE_ON_FAILURE"
-#         Capabilities   = "CAPABILITY_AUTO_EXPAND,CAPABILITY_IAM"
-#         OutputFileName = "CreateStackOutput.json"
-#         StackName      = "MyStack"
-#         TemplatePath   = "build_output::sam-templated.yaml"
-#       }
-#     }
-#   }
-# }
+resource "aws_s3_bucket" "codepipeline_bucket_artifact_store" {}
 
-# resource "aws_codestarconnections_connection" "example" {
-#   name          = "example-connection"
-#   provider_type = "GitHub"
-# }
 
-# resource "aws_s3_bucket" "codepipeline_bucket" {
+data "aws_iam_policy_document" "codepipeline_assume_role" {
+  statement {
+    effect = "Allow"
 
-# }
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
 
-# resource "aws_s3_bucket_public_access_block" "codepipeline_bucket_pab" {
-#   bucket = aws_s3_bucket.codepipeline_bucket.id
+    actions = ["sts:AssumeRole"]
+  }
+}
 
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+resource "aws_iam_role" "codepipeline_role" {
+  assume_role_policy = data.aws_iam_policy_document.codepipeline_assume_role.json
+}
 
-# data "aws_iam_policy_document" "assume_role" {
-#   statement {
-#     effect = "Allow"
+data "aws_iam_policy_document" "codepipeline_policy" {
+  statement {
+    effect = "Allow"
 
-#     principals {
-#       type        = "Service"
-#       identifiers = ["codepipeline.amazonaws.com"]
-#     }
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:PutObjectAcl",
+      "s3:PutObject",
+    ]
 
-#     actions = ["sts:AssumeRole"]
-#   }
-# }
+    resources = [
+      aws_s3_bucket.codepipeline_bucket_artifact_store.arn,
+      "${aws_s3_bucket.codepipeline_bucket_artifact_store.arn}/*"
+    ]
+  }
 
-# resource "aws_iam_role" "codepipeline_role" {
-#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
-# }
+  statement {
+    effect = "Allow"
 
-# data "aws_iam_policy_document" "codepipeline_policy" {
-#   statement {
-#     effect = "Allow"
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild",
+    ]
 
-#     actions = [
-#       "s3:GetObject",
-#       "s3:GetObjectVersion",
-#       "s3:GetBucketVersioning",
-#       "s3:PutObjectAcl",
-#       "s3:PutObject",
-#     ]
+    resources = ["*"]
+  }
 
-#     resources = [
-#       aws_s3_bucket.codepipeline_bucket.arn,
-#       "${aws_s3_bucket.codepipeline_bucket.arn}/*"
-#     ]
-#   }
+  statement {
+    effect = "Allow"
 
-#   statement {
-#     effect = "Allow"
+    actions = [
+      "codecommit:CancelUploadArchive",
+      "codecommit:GetBranch",
+      "codecommit:GetCommit",
+      "codecommit:GetUploadArchiveStatus",
+      "codecommit:UploadArchive"
+    ]
 
-#     actions = [
-#       "codebuild:BatchGetBuilds",
-#       "codebuild:StartBuild",
-#     ]
+    resources = ["*"]
+  }
+}
 
-#     resources = ["*"]
-#   }
-# }
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name   = "codepipeline_policy"
+  role   = aws_iam_role.codepipeline_role.id
+  policy = data.aws_iam_policy_document.codepipeline_policy.json
+}
 
-# resource "aws_iam_role_policy" "codepipeline_policy" {
-#   name   = "codepipeline_policy"
-#   role   = aws_iam_role.codepipeline_role.id
-#   policy = data.aws_iam_policy_document.codepipeline_policy.json
-# }
+resource "aws_cloudwatch_event_rule" "trigger" {
+  name        = "trigger-pipeline"
+  description = "Triggers the pipeline upon a commit to the 'master' branch"
 
+  event_pattern = jsonencode({
+    source = [
+      "aws.codecommit"
+    ]
+    detail-type = [
+      "CodeCommit Repository State Change"
+    ]
+    resources = [
+      aws_codecommit_repository.source_code.arn
+    ]
+    detail = {
+      event = [
+        "referenceCreated",
+        "referenceUpdated"
+      ]
+      referenceType = [
+        "branch"
+      ]
+      referenceName = [
+        "master"
+      ]
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "trigger" {
+  rule      = aws_cloudwatch_event_rule.trigger.name
+  target_id = "codepipeline"
+  arn       = aws_codepipeline.codepipeline.arn
+  role_arn  = aws_iam_role.event_role.arn
+}
+
+data "aws_iam_policy_document" "event_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "event_role" {
+  assume_role_policy = data.aws_iam_policy_document.event_assume_role.json
+}
+
+data "aws_iam_policy_document" "event_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "codepipeline:StartPipelineExecution"
+    ]
+
+    resources = [
+      aws_codepipeline.codepipeline.arn
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "event_policy" {
+  name   = "codepipeline_event_policy"
+  role   = aws_iam_role.event_role.id
+  policy = data.aws_iam_policy_document.event_policy.json
+}
